118fded954f7ffb8fabc52ff5a60ba70c51468ec
Zain Ali <63121360+zain-ali-12@users.noreply.github.com>
2023-03-17 14:25:10 +0400

    Added command line arguments to select debug
    Signed-off-by: Zain Ali <63121360+zain-ali-12@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from website import create_app
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action='store_true')
+    args = parser.parse_args()
     app = create_app()
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(debug=args.debug is not None, host="0.0.0.0", port=80)
